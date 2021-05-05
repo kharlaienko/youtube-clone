93a01f9391288d33c44cbad78685c5d52ac058c3
kharlaienko <kharlayenko@gmail.com>
2021-05-05 19:59:37 +0300

    added VideoItem and DropDownApps, DropDownSetting components

--- src/components/TheCategories.vue
+++ src/components/TheCategories.vue
@@ -6,112 +6,44 @@
 			<div
 				class="py-3 flex space-x-3 overflow-auto text-sm whitespace-nowrap"
 			>
-				<a
+				<a v-for="categoryName in categoriesNames" :key="categoryName"
 					href="#"
 					class="px-3 py-1 transition bg-gray-600 border border-gray-700 rounded-full hover:bg-gray-500 text-white"
-					>All</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Trucks</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Tools</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Machines</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Engines</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Snow</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Ships</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Roads</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Tanks</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Building</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Mars</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Woodworking</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Tractors</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Songs</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>TV Shows</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Football</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Planes</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Live</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Streets</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Math</a
-				>
-				<a
-					href="#"
-					class="px-3 py-1 transition bg-gray-100 border border-gray-300 rounded-full hover:bg-gray-200"
-					>Programming</a
+					>{{ categoryName }}</a
 				>
 			</div>
 		</div>
 	</section>
 </template>
+
+<script>
+export default {
+	data(){
+		return{
+			categoriesNames: [
+				'All',
+				'Trucks',
+				'Tools',
+				'Machines',
+				'Engines',
+				'Snow',
+				'Ships',
+				'Roads',
+				'Tanks',
+				'Building',
+				'Mars',
+				'Woodworking',
+				'Tractors',
+				'Songs',
+				'TV Show',
+				'Football',
+				'Planes',
+				'Live',
+				'Streets',
+				'Math',
+				'Programming',
+			]
+		}
+	}
+}
+</script>
