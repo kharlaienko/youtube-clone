f495b5275f4afffa7cd8f89e392f58a2aac1b3ad
kharlaienko <kharlayenko@gmail.com>
2021-05-05 19:07:43 +0300

    Made basic copmonents.

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<the-header />
+	<the-small-sidebar/>
+	<the-sidebar/>
+	<the-mobile-sidebar/>
+	<the-categories/>
+	<the-videos/>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import TheSmallSidebar from "./components/TheSmallSidebar.vue";
+import TheSidebar from "./components/TheSidebar.vue";
+import TheMobileSidebar from "./components/TheMobileSidebar.vue";
+import TheCategories from "./components/TheCategories.vue";
+import TheVideos from "./components/TheVideos.vue";
+
+export default {
+	components: {
+		TheHeader,
+		TheSmallSidebar,
+		TheSidebar,
+		TheMobileSidebar,
+		TheCategories,
+		TheVideos
+	},
+};
+</script>
