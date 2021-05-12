79cd7cac4584e3ac87c6bb73bbf21d00432a01de
kharlaienko <kharlayenko@gmail.com>
2021-05-12 20:26:17 +0300

    Created BaseIcon for icons. Change icons in header to BaseIcon

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,29 @@
+<template>
+	<svg
+		:class="$attrs.class || 'w-6 h-6'"
+		fill="currentColor"
+		viewBox="0 0 20 20"
+		xmlns="http://www.w3.org/2000/svg"
+        v-html="iconPath"
+	>
+	</svg>
+</template>
+
+<script>
+import icons from '../icons'
+
+export default {
+    props: {
+        iconName: {
+            type: String,
+            required: true
+        }
+    },
+
+    computed: {
+        iconPath(){
+            return icons[this.iconName]
+        }
+    }
+};
+</script>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,18 +3,7 @@
 		<div class="lg:w-1/4 flex">
 			<div class="flex items-center xl:w-64 xl:bg-white pl-4">
 				<button class="mr-3 sm:ml-2 sm:mr-6 focus:outline-none">
-					<svg
-						class="w-6 h-6"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							fill-rule="evenodd"
-							d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-							clip-rule="evenodd"
-						></path>
-					</svg>
+					<base-icon iconName="menu"/>
 				</button>
 				<main-logo/>
 			</div>
@@ -29,57 +18,17 @@
 			class="flex items-center justify-end lg:w-1/4 sm:space-x-3 p-2 sm:px-4"
 		>
 			<button class="sm:hidden p-2 focus:outline-none">
-				<svg
-					class="w-5 h-5"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z"
-						clip-rule="evenodd"
-					></path>
-				</svg>
+				<base-icon iconName="voice" class="w-5 h-5"/>
 			</button>
 			<button class="sm:hidden p-2 focus:outline-none">
-				<svg
-					class="w-5 h-5"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-						clip-rule="evenodd"
-					></path>
-				</svg>
+				<base-icon iconName="search" class="w-5 h-5"/>
 			</button>
 			<button class="relative group p-2 focus:outline-none">
-				<svg
-					class="w-5 h-5"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
-					></path>
-				</svg>
+				<base-icon iconName="viewGrid" class="w-5 h-5"/>
 		<!-- 		<drop-down-apps/> -->
 			</button>
 			<button class="relative group p-2 focus:outline-none">
-				<svg
-					class="w-5 h-5"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"
-					></path>
-				</svg>
+				<base-icon iconName="threeDots" class="w-5 h-5"/>
 			<!-- 	<drop-down-settings/> -->
 			</button>
 			<button-sign-in/>
@@ -94,6 +43,7 @@ import MainLogo from './MainLogo.vue'
 import TheSearch from './TheSearch.vue'
 import ButtonSignIn from './ButtonSignIn.vue'
 import TheVoiceSearch from './TheVoiceSearch.vue'
+import BaseIcon from './BaseIcon.vue'
 
 export default {
 	components:{
@@ -102,7 +52,8 @@ export default {
 		MainLogo,
 		TheSearch,
 		TheVoiceSearch,
-		ButtonSignIn
+		ButtonSignIn,
+		BaseIcon
 	}
 }
 </script>
