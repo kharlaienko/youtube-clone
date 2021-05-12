8867bacf9bba1cfdc019727302385600b6e24a63
kharlaienko <kharlayenko@gmail.com>
2021-05-12 16:59:25 +0300

    Divide header into Search, MainLogo Components

---
+++ src/components/TheSearch.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="flex w-full h-full">
+        <the-search-input/>
+        <the-search-button/>
+	</div>
+</template>
+
+<script>
+import TheSearchInput from './TheSearchInput.vue'
+import TheSearchButton from './TheSearchButton.vue'
+
+
+export default {
+    components: {
+        TheSearchInput,
+        TheSearchButton
+    }
+}
+</script>
